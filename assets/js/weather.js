b9e08481264f7151f2eeebc55e194a47c334a849
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-11-21 15:30:22 -0500

    fixed the html to have CSS appear mostly correct.

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,6 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl= document.querySelector('#searchCity');
 
+// Get information from API
 var getCity= function(city) {
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city +
     "&units=imperial&appid=184b90f195e0b6670ef9fee34b9291e1")
@@ -13,6 +14,7 @@ var getCity= function(city) {
     }
 getCity("city")
 
+//Enable the the user t to select their own city, previously had this hardcoded to prove it worked
 var formSubmitHandler = function(event) {
   event.preventDefault();
 
@@ -24,10 +26,15 @@ var formSubmitHandler = function(event) {
       cityInputEl.value = "";
   }
   else {
+    //if they failed to put in a city, gave them user feedback
       alert("Please enter a city");
   }
   
   console.log(event);
 };
 
+//Display the Current/ 5 day forecast
+
+
+/// event handler to start the action
 userFormEl.addEventListener("submit", formSubmitHandler);
