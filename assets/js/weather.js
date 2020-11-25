1b39fc82c4005d14a72b5a0f7092a63d4c910439
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-11-24 23:13:42 -0500

    took a shot at updating the JS (focusing on API and display of data)

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,4 +1,74 @@
-var userFormEl = document.querySelector("#user-form");
+var lat;
+var lon;
+var searches = [];
+
+var displayWeatherInfo = function(city) {
+
+  var apiWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=184b90f195e0b6670ef9fee34b9291e1";
+  
+  // get today's weather data
+  fetch(apiWeather).then(function (response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function (weatherData) {
+        console.log(weatherData);
+        lat = weatherData.coord.lat;
+        lon = weatherData.coord.lon;
+
+        // Update the current day
+        var dateAndTime = weatherData.name + " " + moment().format("(M/D/YYYY)");
+        $("#city-name").html(dateAndTime + "<img src='http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png'>");
+
+        var temperature = "Temperature: " + weatherData.main.temp + "°F";
+        $("#temperature").text(temperature);
+
+        var humidity = "Humidity: " + weatherData.main.humidity + "%";
+        $("#humidity").text(humidity);
+
+        var windSpeed = "Wind Speed: " + weatherData.wind.speed + " mph";
+        $("#wind-speed").text(windSpeed);
+
+        var onecallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&units=imperial&appid=184b90f195e0b6670ef9fee34b9291e1";
+
+        var uvAPI = "https://api.openweathermap.org/data/2.5/uvi?lat=" + weatherData.coord.lat + "&lon=" + weatherData.coord.lon + "&appid=184b90f195e0b6670ef9fee34b9291e1";
+
+        // 5 day forcast
+        fetch(onecallURL).then(function(response) {
+          // request was successful
+          if (response.ok) {
+            response.json().then(function (forecastData) {
+              for (var i = 1; i <= 5; i++) {
+                dayId = "#day" + (i);
+                $(dayId).find("h3").text(moment.unix(forecastData.daily[i].dt).format("M/D/YYYY"));
+                $(dayId).find("img").attr("src", "http://openweathermap.org/img/wn/" + forecastData.daily[i].weather[0].icon + "@2x.png");
+                $(dayId).find(".temp").text("Temp: " + forecastData.daily[i].temp.day + " F°");
+                $(dayId).find(".humid").text("Humidity: " + forecastData.daily[i].humidity + "%");
+              }
+            });
+          }
+        });
+        
+        // uv data and display
+        fetch(uvAPI).then(function(response) {
+          if (response.ok) {
+            response.json().then(function (uvData) {
+              var uvIndex = uvData.value;
+                if (uvIndex >= 8) {
+                  $("#uv-index").html("UV Index: <span class='bg-danger'>" + uvIndex + "</span>");
+                } else if (uvIndex >= 3) {
+                  $("#uv-index").html("UV Index: <span class='bg-warning'>" + uvIndex + "</span>");
+                } else {
+                  $("#uv-index").html("UV Index: <span class='bg-success'>" + uvIndex + "</span>");
+                }
+            });
+          }
+        });
+      });
+    }
+  });
+};
+
+/*var userFormEl = document.querySelector("#user-form");
 var cityInputEl= document.querySelector('#search-city');
 var currentCityTemp= document.getElementById("curren-city-temp");
 var currentCityMain=document.getElementById("current-city-title");
@@ -9,7 +79,7 @@ var forecastCardDeck=document.getElementById("forcast-card-container")
 var currentDateEl= document.getElementById("featured")
 var firstDayEl=document.getElementById("first-day");
 
-/*/Get the current Weather
+Get the current Weather
 currentWeather= function(city){
 fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city +
   "&units=imperial&appid=184b90f195e0b6670ef9fee34b9291e1")
@@ -20,7 +90,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city +
       console.log(data)
 });
 }
-/**/ 
+
 var displayCurrentWeather= function(data) {
 //   currentCityMain.textContent= "City " + "(" + currentDateEl + ")";;
 var temp = data.main.temp;
@@ -67,7 +137,7 @@ var getUVData = function(coordinates) {
   });
 }
 getUVData("coordinates");
-*/
+
 
 // Get information from API
 var getForecast= function() {
@@ -117,3 +187,4 @@ var displayForecast= function(data) {
 }
 /// event handler to start the action
 userFormEl.addEventListener("submit", selectedCity);
+*/
